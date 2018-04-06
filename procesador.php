9a8ce0f8bf0be73ae9b998da954414fb601e4220
hernandez1999 <36080432+hernandez1999@users.noreply.github.com>
2018-04-05 23:34:43 -0500

    Update procesador.php

--- procesador.php
+++ procesador.php
@@ -7,20 +7,22 @@ $conexion = mysqli_connect("localhost", "root", "mysql2018", "ud", "3306");
 $cadena = "SELECT usuario, clave FROM usuario";
 
 //Guardar el registro 
-
-$resultado=mysqli_query($conexion, $cadena);
-
-if(mysqli_num_rows($resultado) >0){
-   while($registro = mysqli_fetch_assoc($resultado))  
-       echo 'Usuario: '.$registro["usuario"];
+ if(mysqli_num_rows($resultado)>0)
+{
+    $registro= mysqli_fetch_assoc($resultado);      
+        if($_REQUEST["usuario"]==$registro["nombre"] && $_REQUEST["clave"]==$registro["clave"])
+        {
+            echo "Bienvenido a Universidad";
+            echo "Le recordamos sus datos: "
+            echo "<br>usuario: ".$registro["nombre"];
+            echo "<br>clave: ".$registro["clave"];
+        }
+        else
+        {
+            echo"Usuario o contraseña incorrecta";
+            echo"<br>Intente de nuevo";
+        }
 }else{
-    echo 'Existen 0 registros';
+    echo"Existen 0 registros";
 }
-
-if($_REQUEST["usuario"] == $registro["usuario"] && $_REQUEST["clave"] == $registro["clave"]){
-        echo 'Ha entrado con exito';
-}
-
-
-
 
